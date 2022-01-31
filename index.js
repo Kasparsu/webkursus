4b918c0c23ccc8f691311eaf7ca4b6b4bb5841b9
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-01-31 19:19:23 +0200

    add string and array functions

--- index.js
+++ index.js
@@ -2,6 +2,30 @@
 var bar;
 console.log(bar);
 
-let foo = 'foo'; // foo, bar, baz
+let foo = 'FOO'; // foo, bar, baz
 bar = 'bar'; // hoisting
+
+const baz = 'afaf';
+console.log(baz);
+//baz = 'gasdasd'; //error 
+
+let text = 'asdasd' + baz + foo;
+
+let text2 = 'asdasd' + baz + foo;
+
+console.log(text[3]);
+console.log(text.length);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.substring(3, text.length-3));
+//console.log(text.normalize());
+
+let array = [1, 2, 3, 4, 5];
+console.log(array);
+array.push(6);
+console.log(array);
+array.push(7, 8, 9);
+console.log(array);
+console.log(array.pop());
+console.log(array);
 
