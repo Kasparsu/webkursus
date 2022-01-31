732e8f49c6824557e2cae229c086908c06fdcd7e
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-01-31 18:08:56 +0200

    var vs let

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-for(let i = 0;i<10;i++){
-    console.log(i);
-}
+//console.log(foo);
+var bar;
+console.log(bar);
 
-for(let i = 10;i>0;i--){
-    console.log(i);
-}
+let foo = 'foo'; // foo, bar, baz
+bar = 'bar'; // hoisting
 
-for(let i = 0;i<100;i+=2){
-    console.log(i);
-}
-
-i = 1;
-while (i < 1000000) {
-    console.log(i);
-    i*=2;
-}
-
-i = 1000000;
-while (i < 1000000) {
-    console.log(i);
-    i*=2;
-}
-
-
-i = 1000000;
-do {
-    console.log(i);
-    i*=2;
-} while(i < 1000000);
-
-let array = ['kalle', 'malle', 'mati', 'kati'];
-
-for(let i = 0; i<array.length; i++){
-    console.log(array[i]);
-}
-
-array.forEach(element => {
-    console.log(element);
-});
