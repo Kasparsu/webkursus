bd7239ae719bc7cf645a79fad0208bf38d595f99
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-02-02 19:19:37 +0200

    pass by refrence vs pass by value

--- index.js
+++ index.js
@@ -1,50 +1,18 @@
-let numbers = [1,2,3,4,5,6,7,8,9,10];
+let num = 2;
 
-let people = [
-    {
-        name: 'kaspar',
-        age: 28
-    },
-    {
-        name: 'martin',
-        age: 42
-    },
-    {
-        name: 'oleg',
-        age: 86
-    },
-    {
-        name: 'jaanus',
-        age: 11
-    },
-    {
-        name: 'gagarin',
-        age: 53
-    },
-];
+let num2 = num;
 
+num2 = 3;
+console.log(num2);
+console.log(num);
 
+let obj = {
+    num: 2
+}
 
-let evenNumbers = numbers.filter(num => num%2===0);
-console.log(evenNumbers);
+let obj2 = JSON.parse(JSON.stringify(obj));
 
-let youngPeople = people.filter(person => person.age < 60);
-console.log(youngPeople);
-let oldPeople = people.filter(person => person.age >= 60);
-console.log(oldPeople);
+obj2.num = 3;
 
-let poweredNumbers = numbers.map(num => num*num);
-console.log(poweredNumbers);
-
-let names = people.map(person => person.name);
-console.log(names);
-
-let ageSum = people.reduce((sum, person) => sum+=person.age, 0);
-console.log(ageSum);
-
-people.sort((personA, personB) => personB.age-personA.age);
-console.log(people);
-
-people.forEach(value => {
-    console.log(value.name);
-});
+console.log(obj);
+console.log(obj2);
