215ca4a0b38a578204005f5d1957a2697097c0f3
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-02-09 21:09:21 +0200

    add modal v-if v-show example

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
         <button class="button is-primary" @click="active=true">Click me</button>
         <button class="button is-primary" @click="active2=true">Click me2</button>
         <modal 
-            :active="active"
+            v-if="active"
             @close="active=false"
             url="https://bulma.io/images/placeholders/1280x960.png"
         >
         </modal>
         <modal 
-            :active="active2"
+            v-show="active2"
             @close="active2=false"
             url="https://placekitten.com/1280/960"
         >
